5c5423b5ae575befa1fabd5b412ba8b8101a431c
Lakshit Singh Rawat <keysmasherr@gmail.com>
2021-04-07 20:39:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+export default class Sketch {
+  constructor({ dom }) {
+    this.time = 0;
+    this.container = dom;
+    this.scene = new THREE.Scene();
+
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.container.appendChild(this.renderer.domElement);
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    this.resize();
+    this.setupEvents();
+    this.addObjects();
+    this.render();
+  }
+
+  setupEvents() {
+    window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  render() {
+    this.time += 0.5;
+
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
+    
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this));
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  addObjects() {
+    this.geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+    this.material = new THREE.MeshNormalMaterial();
+    this.material = new THREE.ShaderMaterial({
+      
+    });
+    
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+    this.scene.add(this.mesh);
+  }
+}
+
+new Sketch({
+  dom: document.getElementById('container')
+});
